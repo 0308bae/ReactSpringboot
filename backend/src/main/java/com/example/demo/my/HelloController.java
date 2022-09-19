28174f7fb26614b996a3f7d10e857c11e67cf6e9
배재익 <0308bae@naver.com>
2022-09-19 23:19:47 +0900

    fix submit event

--- backend/src/main/java/com/example/demo/my/HelloController.java
+++ backend/src/main/java/com/example/demo/my/HelloController.java
@@ -1,14 +1,10 @@
 package com.example.demo.my;
 
 import com.example.demo.my.board.BoardService;
-import com.example.demo.my.dto.BoardDto;
 import com.example.demo.my.dto.BoardSaveDto;
 import com.example.demo.my.dto.HelloDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
 @RequiredArgsConstructor
 @RestController
 public class HelloController {
