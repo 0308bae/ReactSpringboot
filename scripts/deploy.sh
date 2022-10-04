58a4fbd3edd92bcad5c8139153023308de86d70f
Jaeik Bae <69972996+0308bae@users.noreply.github.com>
2022-10-05 02:00:59 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q)
 echo "start docker-compose up: ubuntu"
 sudo systemctl start docker
 cd /home/ec2-user/github_action
