8d6072a3e42494b56f76a27d40eab85d326a5f0a
JaeikBae <jaeik@DESKTOP-B2K1J8T.localdomain>
2022-10-09 04:00:45 +0900

    update zero downtime service

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-docker rmi $(docker images -q)
-echo "start docker-compose up: ubuntu"
-sudo systemctl start docker
-cd /home/ec2-user/github_action
-sudo docker-compose up --build -d
+
+IS_DB_UP=$(docker ps -q -f name=db)
+if [ -z "$IS_DB_UP" ]; then
+    echo "init up"
+    docker compose -f docker-compose.yaml up --build -d
+    exit
+fi
+
+IS_BLUE_UP=$(docker ps -q -f name=backend-blue)
+
+if [ -z "$IS_BLUE_UP" ]; then
+    echo "blue up"
+    docker compose -f docker-compose-blue.yaml up --build -d
+    BEFORE_COMPOSE_COLOR="green"
+    AFTER_COMPOSE_COLOR="blue"
+else
+    echo "green up"
+    docker compose -f docker-compose-green.yaml up --build -d
+    BEFORE_COMPOSE_COLOR="blue"
+    AFTER_COMPOSE_COLOR="green"
+fi
+ 
+echo "Load complete. Start switching after 60 sec"
+sleep 60
+
+IS_EXIST_AFTER=$(docker ps | grep backend-${AFTER_COMPOSE_COLOR})
+if [ -n "$IS_EXIST_AFTER" ]; then
+    docker exec -it nginx cp /etc/nginx/conf.d/${AFTER_COMPOSE_COLOR}.conf /etc/nginx/conf.d/default.conf
+    docker exec -it nginx nginx -s reload
+    docker compose -f docker-compose-${BEFORE_COMPOSE_COLOR}.yaml down
+    echo "$BEFORE_COMPOSE_COLOR down"
+fi
