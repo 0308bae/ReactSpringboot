b747df9fe0a0475d699b19844e230b27fc905f3f
Jaeik Bae <69972996+0308bae@users.noreply.github.com>
2022-10-09 12:32:25 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+sudo systemctl start docker
 IS_DB_UP=$(docker ps -q -f name=nginx)
 if [ -z "$IS_DB_UP" ]; then
     echo "init up"
+    cd /home/ec2-user/github_action
     docker-compose -f docker-compose.yaml up --build -d
     exit
 fi
@@ -11,11 +12,13 @@ IS_BLUE_UP=$(docker ps -q -f name=backend-blue)
 
 if [ -z "$IS_BLUE_UP" ]; then
     echo "blue up"
+    cd /home/ec2-user/github_action
     docker-compose -f docker-compose-blue.yaml up --build -d
     BEFORE_COMPOSE_COLOR="green"
     AFTER_COMPOSE_COLOR="blue"
 else
     echo "green up"
+    cd /home/ec2-user/github_action
     docker-compose -f docker-compose-green.yaml up --build -d
     BEFORE_COMPOSE_COLOR="blue"
     AFTER_COMPOSE_COLOR="green"
@@ -28,6 +31,7 @@ IS_EXIST_AFTER=$(docker ps | grep backend-${AFTER_COMPOSE_COLOR})
 if [ -n "$IS_EXIST_AFTER" ]; then
     docker exec -it nginx cp /etc/nginx/conf.d/${AFTER_COMPOSE_COLOR}.conf /etc/nginx/conf.d/default.conf
     docker exec -it nginx nginx -s reload
+    cd /home/ec2-user/github_action
     docker-compose -f docker-compose-${BEFORE_COMPOSE_COLOR}.yaml down
     echo "$BEFORE_COMPOSE_COLOR down"
 fi
